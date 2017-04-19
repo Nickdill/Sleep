66a35e1b71d7d017fd3c10f118f2c7789bfaa85e
Nick Dill <nickdill@berkeley.edu>
2017-04-18 21:47:39 -0700

    Fixed disqus. Added rangebar in landing content.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $(window).on('scroll', function(){
         var stop = Math.round($(window).scrollTop());
         var ratio = stop / ($(document).height() - 400);
-        console.log($(document).height());
         var color = 'rgba(0,0,0,'+ratio+')';
         $('.main').css({
             'background' : color
