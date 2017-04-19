c0f6d44cb94f7b045e8e700da4fd39de5cb3f0bd
Nick Dill <nickdill@berkeley.edu>
2017-04-18 20:35:30 -0700

    Added disqus and updated ui

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $(window).on('scroll', function(){
+        var stop = Math.round($(window).scrollTop());
+        var ratio = stop / ($(document).height() - 400);
+        console.log($(document).height());
+        var color = 'rgba(0,0,0,'+ratio+')';
+        $('.main').css({
+            'background' : color
+        });
+        var r = 241 + ratio*2;
+        var g = 196 - ratio*40;
+        var b = 15 + ratio*3;
+        var st = 'rgba('+r+','+g+','+b+',1.0)';
+        $(".text").css({
+            'color': st
+        });
+    });
+
+});
