a64bbb18c83565d6f3bab96b17338046cd00bda3
Nick Dill <nickdill@berkeley.edu>
2017-04-23 22:31:32 -0700

    Updated style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $(window).on('scroll', function(){
         var stop = Math.round($(window).scrollTop());
         var ratio = stop / 600;
-        var color = 'rgba(0,0,0,'+ratio+')';
+        var color = 'rgba(20,20,30,'+ratio+')';
         $('.main').css({
             'background' : color
         });
